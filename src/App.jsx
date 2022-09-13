fa1858a2c6a6e177b52164e30081915432211758
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-09-13 11:31:09 +0200

    Grundprojekt med parcel react och react-dom

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+    return (
+        <div>App</div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+)
