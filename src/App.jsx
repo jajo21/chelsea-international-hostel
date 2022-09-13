0e0e13d664645737c2dabe0a5a702af9287e2895
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-09-13 11:55:00 +0200

    React router dom tillagt tillsammans med enklare routing navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+    HashRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+
+import Navbar from './components/Navbar';
+import Home from './routes/Home';
+import Start from './routes/Start';
+import Climate from './routes/Climate';
 
 function App() {
     return (
-        <div>App</div>
+        <>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Start />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/climate" element={<Climate />} />
+            </Routes>
+        </>
     )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <React.StrictMode>
+        <HashRouter>
+            <App />
+        </HashRouter>
+    </React.StrictMode>
 )
