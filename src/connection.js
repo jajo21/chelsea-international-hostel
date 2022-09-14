e41d929c01533edef8c491cdf008298edee61a43
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-09-14 12:39:58 +0200

    Sparar en början på realtidskommunikationen. Har lagt till paketet @microsoft/signalr

---
+++ src/connection.js
@@ -0,0 +1,11 @@
+import * as signalR from "@microsoft/signalr";
+
+export const initializeSignalRConnection = (url, token) => {
+    const connection = new signalR.HubConnectionBuilder()
+        .withUrl(url, { accessTokenFactory: () => token })
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    connection.start().catch(err => console.error(err.toString()));
+    return connection;
+}
