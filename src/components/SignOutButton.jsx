1f21476feb41b5e8b3419765c6f9cd4a7ba2915d
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-09-15 13:27:56 +0200

    Buggfix logout

--- src/components/SignOutButton.jsx
+++ src/components/SignOutButton.jsx
@@ -3,11 +3,11 @@ import { useMsal } from "@azure/msal-react";
 import { handleLogout } from "../data/auth/handleAuth";
 
 export const SignOutButton = () => {
-  const { instance } = useMsal();
+    const { instance } = useMsal();
 
-  return (
-    <div className="signout-button">
-      <button onClick={() => handleLogout(instance)}>Logga ut</button>
-    </div>
-  );
+    return (
+        <div className="signout-button">
+            <button onClick={() => handleLogout(instance)}>Logga ut</button>
+        </div>
+    );
 };
