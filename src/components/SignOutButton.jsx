9f0cc773d5a791a09e0503853309274ac126337b
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-09-15 09:45:56 +0200

    Fixade konflikter

--- src/components/SignOutButton.jsx
+++ src/components/SignOutButton.jsx
@@ -1,24 +1,14 @@
 import React from "react";
 import { useMsal } from "@azure/msal-react";
+import { handleLogout } from "../auth/handleAuth";
 
-function handleLogout(instance) {
-  instance.logoutRedirect().catch((e) => {
-    console.error(e);
-  });
-}
 
-/**
- * Renders a button which, when selected, will open a popup for logout
- */
 export const SignOutButton = () => {
   const { instance } = useMsal();
 
   return (
-    <div>
-      <button className="signoutButton" onClick={() => handleLogout(instance)}>
-        <i className="fa-solid fa-right-from-bracket"></i>
-        Logga ut
-      </button>
+    <div className="signout-button">
+      <button onClick={() => handleLogout(instance)}>Logga ut</button>
     </div>
   );
 };
