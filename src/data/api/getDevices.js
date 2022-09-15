4cc33fb7c0386c50e85dd806fdfa631e9a3faee4
Johannes Jakobsson <jajo21pg@student.ju.se>
2022-09-15 11:58:17 +0200

    Fixat så man får ner alla sensorer till klimatsidan

--- src/data/api/getDevices.js
+++ src/data/api/getDevices.js
@@ -1,6 +1,6 @@
 const smartHutEndpoint = "https://api.smarthut.se";
 
-export async function getMyBuilding(accessToken) {
+export async function getBuilding(accessToken) {
     const headers = new Headers();
     const url = `${smartHutEndpoint}/BuildingInfo/GetMyBuilding`
 
@@ -14,5 +14,23 @@ export async function getMyBuilding(accessToken) {
 
     return fetch(url, options)
         .then(response => response.json())
+        .catch(error => console.log(error));
+}
+
+export async function getBuildingDevices(accessToken, buildingId) {
+    const headers = new Headers();
+    const url = `${smartHutEndpoint}/BuildingInfo/${buildingId}/true`
+
+    headers.append("Authorization", "Bearer " + accessToken);
+    headers.append('Content-type', 'application/json; charset=UTF-8')
+
+    const options = {
+        method: "GET",
+        headers: headers
+    };
+
+    return fetch(url, options)
+        .then(response => response.json())
+        .then(res => res.devices)
         .catch(error => console.log(error));
 }
